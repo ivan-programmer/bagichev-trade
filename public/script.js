fb91183300429e6889f12cc0c9061275bcb71237
Ivan Vinogradov <programming.game@ya.ru>
2017-09-03 16:43:04 +0300

    modified javascript code

--- public/script.js
+++ public/script.js
@@ -1,72 +1,82 @@
 
 /* Script by Ivan Vinogradov for Bagichev Trade */
-var doc, handlers, main, onStart, preloader, tabHandler, tabs;
 
-doc = document;
+(function() {
+  var doc, handlers, main, onStart, preloader, tabHandler, tabs;
 
-$(function() {
-  return main();
-});
+  doc = document;
 
-main = function() {
-  tabs();
-  handlers();
-  return onStart();
-};
-
-tabs = function() {
-  tabs = ['manufacturer', 'production', 'contacts'];
-  tabs.forEach(tabHandler(this), this);
-  return $('#name').on('click', function() {
-    var activeName, activeTabs;
-    activeTabs = doc.querySelectorAll('.tab-active');
-    activeName = doc.querySelector('#name-active');
-    main = doc.querySelector('main');
-    activeTabs.forEach(this.style.opacity = 0, this);
-    activeName.style.opacity = 1;
-    return $.ajax({
-      url: './pages/main.php',
-      cache: false,
-      success: function(page) {
-        return main.innerHTML = page;
-      },
-      beforeSend: function() {
-        return preloader();
-      }
-    });
+  $(function() {
+    return main();
   });
-};
 
-handlers = function() {};
+  main = function() {
+    tabs();
+    handlers();
+    return onStart();
+  };
 
-onStart = function() {
-  return $('#name').trigger('click');
-};
-
-tabHandler = function(name) {
-  return $('#' + name).on('click', function() {
-    var activeName, activeTabs;
-    activeName = doc.querySelector('#name-active');
-    activeTabs = doc.querySelectorAll('.tab-active');
-    main = doc.querySelector('main');
-    activeName.style.opacity = 0;
-    activeTabs.forEach(this.style.opacity = 0, this);
-    doc.querySelector('#' + name + ' .tab-active').style.opacity = 1;
-    return $.ajax({
-      url: './pages/' + name + '.php',
-      cache: false,
-      type: 'GET',
-      success: function(page) {
-        return main.innerHTML = page;
-      },
-      beforeSend: function() {
-        return preloader();
-      }
+  tabs = function() {
+    tabs = ['manufacturer', 'production', 'contacts'];
+    tabs.forEach(function(tab) {
+      return tabHandler(tab);
     });
-  });
-};
+    return $('#name').on('click', function() {
+      var activeName, activeTabs;
+      activeTabs = doc.querySelectorAll('.tab-active');
+      activeName = doc.querySelector('#name-active');
+      main = doc.querySelector('main');
+      activeTabs.forEach(function(tab) {
+        return tab.style.opacity = 0;
+      });
+      activeName.style.opacity = 1;
+      return $.ajax({
+        url: './pages/main.php',
+        cache: false,
+        success: function(page) {
+          return main.innerHTML = page;
+        },
+        beforeSend: function() {
+          return preloader();
+        }
+      });
+    });
+  };
 
-preloader = function() {};
+  handlers = function() {};
+
+  onStart = function() {
+    return $('#name').trigger('click');
+  };
+
+  tabHandler = function(name) {
+    return $('#' + name).on('click', function() {
+      var activeName, activeTabs;
+      activeName = doc.querySelector('#name-active');
+      activeTabs = doc.querySelectorAll('.tab-active');
+      main = doc.querySelector('main');
+      activeName.style.opacity = 0;
+      activeTabs.forEach(function(tab) {
+        return tab.style.opacity = 0;
+      });
+      doc.querySelector('#' + name + ' .tab-active').style.opacity = 1;
+      return $.ajax({
+        url: './pages/' + name + '.php',
+        cache: false,
+        type: 'GET',
+        success: function(page) {
+          return main.innerHTML = page;
+        },
+        beforeSend: function() {
+          return preloader();
+        }
+      });
+    });
+  };
+
+  preloader = function() {};
 
 
-/* Made by Ivan Vinogradov for Bagichev Trade */
+  /* Made by Ivan Vinogradov for Bagichev Trade */
+
+}).call(this);
